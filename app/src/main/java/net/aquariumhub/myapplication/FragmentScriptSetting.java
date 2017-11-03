1633c4790b89cb0adda563fe918d2f1da5e08bbf
Yukine <michael_7150@yahoo.com.tw>
2017-11-03 19:53:53 +0800

    Add FragmentScriptSetting

---
+++ app/src/main/java/net/aquariumhub/myapplication/FragmentScriptSetting.java
@@ -0,0 +1,54 @@
+package net.aquariumhub.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.borax12.materialdaterangepicker.time.RadialPickerLayout;
+
+public class FragmentScriptSetting extends Fragment implements com.borax12.materialdaterangepicker.time.TimePickerDialog.OnTimeSetListener, com.borax12.materialdaterangepicker.date.DatePickerDialog.OnDateSetListener{
+  /*
+  Calendar now = Calendar.getInstance();
+
+  com.borax12.materialdaterangepicker.date.DatePickerDialog dpd = com.borax12.materialdaterangepicker.date.DatePickerDialog.newInstance(
+          FragmentTabSetting.this,
+          now.get(Calendar.YEAR),
+          now.get(Calendar.MONTH),
+          now.get(Calendar.DAY_OF_MONTH));
+
+  TextView tv_time;
+  */
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.tab_setting, container, false);
+  }
+
+  @Override
+  public void onViewCreated(View view, Bundle savedInstanceState) {
+
+    super.onViewCreated(view, savedInstanceState);
+
+    // dpd.show(getActivity().getFragmentManager(), "Datepickerdialog");
+  }
+
+
+  @Override
+  public void onDateSet(com.borax12.materialdaterangepicker.date.DatePickerDialog view, int year, int monthOfYear, int dayOfMonth, int yearEnd, int monthOfYearEnd, int dayOfMonthEnd) {
+
+    String time = "You picked the following time: From ";
+
+  }
+
+  @Override
+  public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute, int hourOfDayEnd, int minuteEnd) {
+    String hourString = hourOfDay < 10 ? "0"+hourOfDay : ""+hourOfDay;
+    String minuteString = minute < 10 ? "0"+minute : ""+minute;
+    String hourStringEnd = hourOfDayEnd < 10 ? "0"+hourOfDayEnd : ""+hourOfDayEnd;
+    String minuteStringEnd = minuteEnd < 10 ? "0"+minuteEnd : ""+minuteEnd;
+    String time = "You picked the following time: From - "+hourString+"h"+minuteString+" To - "+hourStringEnd+"h"+minuteStringEnd;
+
+  }
+}
--- app/src/main/java/net/aquariumhub/myapplication/FragmentTabSetting.java
+++ app/src/main/java/net/aquariumhub/myapplication/FragmentTabSetting.java
@@ -1,7 +1,9 @@
 package net.aquariumhub.myapplication;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
@@ -14,11 +16,37 @@ import com.borax12.materialdaterangepicker.time.RadialPickerLayout;
 
 import java.util.Calendar;
 
-public class FragmentTabSetting extends Fragment {
+public class FragmentTabSetting extends DialogFragment {
 
   ImageButton setting_edit_temp;
   ImageButton setting_edit_bright;
   ImageButton setting_edit_frequency;
+
+  /* The activity that creates an instance of this dialog fragment must
+     * implement this interface in order to receive event callbacks.
+     * Each method passes the DialogFragment in case the host needs to query it. */
+  public interface NoticeDialogListener {
+    public void onDialogPositiveClick(DialogFragment dialog);
+    public void onDialogNegativeClick(DialogFragment dialog);
+  }
+
+  // Use this instance of the interface to deliver action events
+  NoticeDialogListener mListener;
+
+  // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+  @Override
+  public void onAttach(Activity activity) {
+    super.onAttach(activity);
+    // Verify that the host activity implements the callback interface
+    try {
+      // Instantiate the NoticeDialogListener so we can send events to the host
+      mListener = (NoticeDialogListener) activity;
+    } catch (ClassCastException e) {
+      // The activity doesn't implement the interface, throw exception
+      throw new ClassCastException(activity.toString()
+              + " must implement NoticeDialogListener");
+    }
+  }
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
